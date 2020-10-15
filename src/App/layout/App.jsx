93f070a7e555e012c7e98c3a1b3ad413021907b8
anasAB <anas.222@hotmail.com>
2020-10-15 09:32:58 +0200

    Events

--- src/App/layout/App.jsx
+++ src/App/layout/App.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../Features/events/eventDashboard/EventDashboard";
 import NavBar from "../../Features/nav/NavBar";
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  //**! select Event*/
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard />
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectEvent}
+          selecteDEvent={selectedEvent}
+        />
       </Container>
     </>
   );
--- src/Features/events/eventForm/EventForm.jsx
+++ src/Features/events/eventForm/EventForm.jsx
@@ -1,38 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
+import cuid from "cuid";
 
-export default function EventForm() {
+export default function EventForm({ setFormOpen, createEvent, selecteDEvent }) {
+  const initialValues = selecteDEvent ?? {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  function handelInputChange(event) {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  }
+
+  function handelFormSubmit() {
+    createEvent({
+      ...values,
+      id: cuid(),
+      hostedBy: "Anas",
+      attendees: [],
+    });
+  }
+
   return (
     <Segment clearing>
-      <Header content="create new Event" />
+      <Header content={selecteDEvent ? "Edit Event" : "create new Event"} />
 
-      <Form>
+      <Form onSubmit={handelFormSubmit}>
         <Form.Field>
-          <input type="text" placeholder="Event Tile" />
+          <input
+            type="text"
+            placeholder="Event Tile"
+            name="title"
+            value={values.title}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Form.Field>
-          <input type="text" placeholder="Category" />
+          <input
+            type="text"
+            placeholder="Category"
+            name="Category"
+            value={values.category}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Form.Field>
-          <input type="text" placeholder="Description" />
+          <input
+            type="text"
+            placeholder="Description"
+            name="Description"
+            value={values.description}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Form.Field>
-          <input type="text" placeholder="City" />
+          <input
+            type="text"
+            placeholder="City"
+            name="City"
+            value={values.city}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Form.Field>
-          <input type="text" placeholder="Venue" />
+          <input
+            type="text"
+            placeholder="Venue"
+            name="Venue"
+            value={values.venue}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Form.Field>
-          <input type="date" placeholder="Date" />
+          <input
+            type="date"
+            placeholder="Date"
+            name="date"
+            value={values.date}
+            onChange={(event) => handelInputChange(event)}
+          />
         </Form.Field>
 
         <Button content="submit" type="submit" floated="right" positive />
-        <Button content="cancel" type="submit" floated="right" />
+        <Button
+          onClick={() => setFormOpen(false)}
+          content="cancel"
+          floated="right"
+        />
       </Form>
     </Segment>
   );

00ebfbdd31920909793eeb3534ec3617862540b0
anasAB <anas.222@hotmail.com>
2020-10-18 11:13:30 +0200

    Routing

--- src/App/layout/App.jsx
+++ src/App/layout/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../Features/events/eventDashboard/EventDashboard";
+import EventDetaildPage from "../../Features/events/eventDetalied/EventDetaildPage";
+import EventForm from "../../Features/events/eventForm/EventForm";
+import HomePage from "../../Features/home/HomePage";
 import NavBar from "../../Features/nav/NavBar";
 
 function App() {
@@ -22,12 +26,17 @@ function App() {
     <>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/events:id" component={EventDetaildPage} />
+        <Route path="/events" component={EventDashboard} />
+        <Route path="/createEvent" component={EventForm} />
+
+        {/* <EventDashboard
           formOpen={formOpen}
           setFormOpen={setFormOpen}
           selectEvent={handleSelectEvent}
           selecteDEvent={selectedEvent}
-        />
+        /> */}
       </Container>
     </>
   );
---
+++ src/Features/events/eventDetalied/EventDetaildPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EventDetaildPage() {
+  return <h1>EventDetaildPage</h1>;
+}
